3e9fe5966e00cd4294360ff69eae4ada2c376339
Cristian Colceriu <contact@ghinda.net>
2012-08-20 17:15:04 +0300

    Build and deploy latest version to stage.

--- build/webapp/core/desktop.js
+++ build/webapp/core/desktop.js
@@ -171,7 +171,7 @@ finfore.desktop = function() {
 			
 			// scroll to tab selector button
 			nodes.$tabletTabsContainer.animate({
-				scrollTop: $tab.position().top
+				scrollTop: $tab.position().top + 250
 			}, 200);
 		}
 
